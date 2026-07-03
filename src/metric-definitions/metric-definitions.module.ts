ad473e808087d613cc5cfb1e69135bf56e2e75ba
AlehandroSV <alehandrosantos99@gmail.com>
2026-07-03 16:55:08 -0300

    fix: add ConsentModule import to MetricDefinitionsModule (missing dependency)

--- src/metric-definitions/metric-definitions.module.ts
+++ src/metric-definitions/metric-definitions.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MetricDefinitionsController } from './metric-definitions.controller';
 import { MetricDefinitionsService } from './metric-definitions.service';
+import { ConsentModule } from '../consent/consent.module';
 
 @Module({
+  imports: [ConsentModule],
   controllers: [MetricDefinitionsController],
   providers: [MetricDefinitionsService],
   exports: [MetricDefinitionsService],
